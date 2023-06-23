073f20206168238759c26003a4d6487865fad118
Vinicius Parede <vinicius.parede1@gmail.com>
2023-06-22 22:25:17 -0300

    initial cnn fine tunning

--- src/CNNFineTunning.py
+++ src/CNNFineTunning.py
@@ -1,64 +1,57 @@
-import tensorflow as tf
-from keras.applications import ResNet50
-from keras.models import Model
-from keras.layers import Dense, GlobalAveragePooling2D
-
 from SPRXRay import SPRXRay
+from keras.applications import ResNet50
+from keras.layers import Dense
+from keras.models import Model
 
-# Load the SPR X-Ray dataset
-data = SPRXRay()
 
-(x_train, y_train), (x_test, y_test) = data.load_data(type_of_predict="gender")
+class XRayModelTrainer:
+    def __init__(self):
+        self.data = SPRXRay()
+        self.base_model = ResNet50(
+            weights="imagenet", include_top=False, input_shape=(224, 224, 3)
+        )
+        self.gender_model = None
+        self.age_model = None
 
-# Normalize the images
-x_train = x_train / 255.0
-x_test = x_test / 255.0
+    def train_gender_model(self):
+        # Add custom layers for gender classification
+        gender_output = Dense(1, activation="sigmoid")(self.base_model.output)
+        self.gender_model = Model(inputs=self.base_model.input, outputs=gender_output)
 
-# Load the ResNet50 model without the top layer (include_top=False)
-base_model = ResNet50(weights="imagenet", include_top=False, input_shape=(256, 256, 3))
+        # Freeze the base layers
+        for layer in self.base_model.layers:
+            layer.trainable = False
 
-# Add a global spatial average pooling layer
-x = base_model.output
-x = GlobalAveragePooling2D()(x)
+        (x_train, y_train), (_, _) = self.data.load_data(type_of_predict="gender")
 
-# Add a fully-connected layer
-x = Dense(1024, activation="relu")(x)
+        # Compile and train the gender classification model
+        self.gender_model.compile(
+            optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"]
+        )
+        self.gender_model.fit(x_train, y_train, epochs=10, batch_size=32)
 
-# Add a logistic layer with 10 classes (for CIFAR-10)
-predictions = Dense(2, activation="softmax")(x)
+        return self.gender_model
 
-# This is the model we will train
-model = Model(inputs=base_model.input, outputs=predictions)
+    def train_age_model(self):
+        # Add custom layers for age regression
+        age_output = Dense(1)(self.base_model.output)
+        self.age_model = Model(inputs=self.base_model.input, outputs=age_output)
 
-# First: train only the top layers (which were randomly initialized)
-# i.e. freeze all convolutional ResNet50 layers
-for layer in base_model.layers:
-    layer.trainable = False
+        # Freeze the base layers
+        for layer in self.base_model.layers:
+            layer.trainable = False
 
-# Compile the model
-model.compile(optimizer="adam", loss="sparse_categorical_crossentropy")
+        (x_train, y_train), (_, _) = self.data.load_data(type_of_predict="age")
 
-# Train the model on the new data for a few epochs
-model.fit(x_train, y_train, epochs=5)
+        # Compile and train the age regression model
+        self.age_model.compile(optimizer="adam", loss="mean_squared_error")
+        self.age_model.fit(x_train, y_train, epochs=10, batch_size=32)
 
-# At this point, the top layers are well trained and we can start fine-tuning
-# convolutional layers from ResNet50. We will freeze the bottom N layers
-# and train the remaining top layers.
+        return self.age_model
 
-# Let's visualize layer names and layer indices to see how many layers we should freeze:
-for i, layer in enumerate(base_model.layers):
-    print(i, layer.name)
+    def train_models(self):
+        return (self.train_gender_model(), self.train_age_model())
+    
+trainer = XRayModelTrainer()
+gender_model, age_model = trainer.train_models()
 
-# We chose to train the top 2 ResNet blocks, i.e. we will freeze
-# the first 165 layers and unfreeze the rest:
-for layer in model.layers[:165]:
-    layer.trainable = False
-for layer in model.layers[165:]:
-    layer.trainable = True
-
-# We need to recompile the model for these modifications to take effect
-model.compile(optimizer="adam", loss="sparse_categorical_crossentropy")
-
-# We train our model again (this time fine-tuning the top 2 ResNet blocks)
-# alongside the top Dense layers
-model.fit(x_train, y_train, epochs=5)
